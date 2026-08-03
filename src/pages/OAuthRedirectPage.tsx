7802f0f416db19c5037f4fa9166d4f3916453ff9
Tyler Katsha <tylerkatsha14@gmail.com>
2026-08-03 11:06:27 +0200

    Configured for netlify

--- src/pages/OAuthRedirectPage.tsx
+++ src/pages/OAuthRedirectPage.tsx
@@ -1,22 +1,20 @@
-// OAuth2Redirect.tsx
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../contexts/UserContext";
 import { HomeSkeleton } from "../skeletons/pages/HomeSkeleton";
 
 export const OAuth2Redirect = () => {
-    const {fetchUser} = useUser();
+    const { fetchUser } = useUser();
     const navigate = useNavigate();
 
+    const run = async () => {
+        await fetchUser();
+        navigate("/");
+    };
+
     useEffect(() => {
-
-        const run = async () => {
-            await fetchUser();
-            navigate("/");     
-        };
-
         run();
     }, []);
 
-    return <HomeSkeleton/>;
+    return <HomeSkeleton />;
 };
