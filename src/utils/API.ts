641618e7d115e2e50c45311a2a5a2dc5d0f80ce5
Tyler Katsha <tylerkatsha14@gmail.com>
2026-08-05 02:11:08 +0200

    Changed API again

--- src/utils/API.ts
+++ src/utils/API.ts
@@ -1,6 +1,6 @@
-const API_DEV = 'http://localhost:8080/api/v1'
+// const API_DEV = 'http://localhost:8080/api/v1'
 const API_PROD = 'https://youthengedi-app-v1-0.onrender.com/api/v1'
-const OAUTH_API_DEV = 'http://localhost:8080/oauth2/authorization'
+// const OAUTH_API_DEV = 'http://localhost:8080/oauth2/authorization'
 const OAUTH_API_PROD = 'https://youthengedi-app-v1-0.onrender.com/oauth2/authorization'
 export const API = API_PROD
 export const OAUTH_API = OAUTH_API_PROD
