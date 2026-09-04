efa117e3501eafeae57063efbf4831f26aa708f9
Tyler Katsha <tylerkatsha14@gmail.com>
2026-09-04 19:08:12 +0200

    minor change

--- src/utils/API.ts
+++ src/utils/API.ts
@@ -1,4 +1,4 @@
-const production = false;
+const production = true;
 const API_DEV = 'http://localhost:8080/api/v1';
 const API_PROD = 'https://youthengedi-app-v1-0.onrender.com/api/v1';
 const OAUTH_API_DEV = 'http://localhost:8080/oauth2/authorization';
